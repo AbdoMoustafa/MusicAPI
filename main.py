3353a7c60fed745063fb57c333a5aa5cbe8ef73c
AbdoMoustafa <abdo.moustafa@ucdconnect.ie>
2022-05-25 21:51:47 +0100

    Added info about albums from an inputted artist

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
+import json
 import requests as r
 import secrets
 
 
 def main():
     artist_name = input("Enter an artist to view their top songs: ")
-    artist_songs = find_artist(artist_name)[0]
-    song_lyrics = input("What song number would you like lyrics for? ")
-    get_lyrics(artist_songs[int(song_lyrics) - 1])
+    songs, artists = search(artist_name)
+    index = int(input("What song number would you like lyrics for? "))
+    get_lyrics(songs[index-1])
+
+    display_albums = int(input("\n\nEnter 1 if you would like to see all the artist's albums, otherwise enter any other number :"))
+
+    if display_albums == 1:
+        print(f"\n\nArtist's Albums/Tapes\n")
+        get_albums(artists[index-1])
     return
 
 
-def find_artist(name):
+def search(name):
     url = "https://genius-song-lyrics1.p.rapidapi.com/search"
 
     querystring = {"q": name, "per_page": "50", "page": "1"}
@@ -31,7 +38,7 @@ def find_artist(name):
         songs.append(hit["result"]["api_path"])
         print(f'{i + 1}. {hit["result"]["full_title"]}')
 
-    return songs, artist_id
+    return songs, artist
 
 
 def get_lyrics(api_path):
@@ -46,7 +53,24 @@ def get_lyrics(api_path):
     obj = response['response']['lyrics']['lyrics']['body']['plain']
     print(obj)
 
-#def get_album():
+
+def get_albums(api_path):
+    url = f"https://genius-song-lyrics1.p.rapidapi.com{api_path}/albums"
+
+    headers = {
+        "X-RapidAPI-Host": "genius-song-lyrics1.p.rapidapi.com",
+        "X-RapidAPI-Key": "8fb30a6f84msh304f7b132f6155cp101c24jsnb5cf49b26e0e"
+    }
+    response = r.get(url, headers=headers).json()
+    #print(response)
+
+    for album in response["response"]["albums"]:
+        print(f"{album['full_title']} \nRelease Date : {album['release_date_components']['day']}/"
+              f"{album['release_date_components']['month']}/"
+              f"{album['release_date_components']['year']}\n"
+              f"Cover Art : {album['cover_art_thumbnail_url']}\n")
+
+
 
 
 if __name__ == '__main__':
